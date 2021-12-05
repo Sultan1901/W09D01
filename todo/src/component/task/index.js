c1c0d58d8efb304aa923b7b05c4ea8014c9a49a9
Sultan1901 <sultan.aqeel.elq.js@tuwaiq.edu.sa>
2021-12-05 21:25:33 +0300

    debuging issues

--- todo/src/component/task/index.js
+++ todo/src/component/task/index.js
@@ -1,40 +1,58 @@
-import axios from 'axios';
-import React from 'react'
+import axios from "axios";
+import React from "react";
 import { useState, useEffect } from "react";
 
 const Task = () => {
-const BASE_URL = process.env.REACT_APP_BASE_URL;
-    const [task, setTask] = useState([]);
-    useEffect(() => {
-     taskshow()
-    }, [])
-    const taskshow = async () => {
-        const result = await axios.get(`${BASE_URL}/gettask`);
-       setTask(result.data);
-        
-    }
-const del = async () => {
-  try { const res = await axios.delete(`${BASE_URL}/delTask/:id`)
-  console.log(res);
+  const BASE_URL = process.env.REACT_APP_BASE_URL;
+  const [task, setTask] = useState([]);
+  useEffect(() => {
+    taskshow()
+    setNewtask();
+  }, []);
+  const taskshow = async () => {
+    const result = await axios.get(`${BASE_URL}/gettask`);
+    setTask(result.data);
+  };
+  const del = async () => {
+    try {
+      const res = await axios.delete(`${BASE_URL}/delTask/:id`);
+      console.log(res);
+    } catch (error) {}
+  };
+  const [newtask, setNewtask] = useState("");
+  const addtask = async () => {
+    try {
+      const res = await axios.post(`${BASE_URL}/createtask`, {
+        name: newtask,
+      });
+    } catch (error) {}
+  };
+
+  return (
+    <div>
+      <h1>Tasks</h1>
+      <input 
+        onChange={(e) => {
+          setNewtask(e.target.value);
+          console.log(e);
     
-  } catch (error) {
-    
-  }
- 
-}
+        }}
+        placeholder="add task"
+       />{" "}
+      <button onClick={addtask}></button>
+      {task.map((e, i) => (
+        <ul>
+          <li>
+            {e.name}
+            <button onClick={del} key={e._id}>
+              delete
+            </button>
+          </li>
+          {console.log(i)}{" "}
+        </ul>
+      ))}
+    </div>
+  );
+};
 
-    return (
-      <div>
-        <h1>Tasks</h1>
-
-        {task.map((e,i) => (
-          <ul>
-            <li>{e.name}<button onClick={del} key={i._id} >delete</button></li>
-          {console.log(i)} </ul>
-         
-        ))}
-      </div>
-    );
-}
-
-export default Task
+export default Task;
