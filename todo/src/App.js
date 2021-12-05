6d1bea6696f9ffe175f29a7b3df34a40914a88b9
Sultan1901 <sultan.aqeel.elq.js@tuwaiq.edu.sa>
2021-12-05 16:43:09 +0300

    show list of tasks

--- todo/src/App.js
+++ todo/src/App.js
@@ -6,6 +6,8 @@ import cors from "cors";
 // import Login from "./component/Login"
 import Register from "./component/Register";
 import Login from "./component/Login";
+import Task from "./component/task";
+
 
 
 import {BrowserRouter as Route , Routes , Link} from "react-router-dom"
@@ -15,10 +17,9 @@ function App() {
 
   return (
     <div>
-
       <Register />
       <Login />
-
+      <Task />
     </div>
   );
 }
--- todo/src/component/task/index.js
+++ todo/src/component/task/index.js
@@ -1,11 +1,34 @@
+import axios from 'axios';
 import React from 'react'
+import { useState, useEffect } from "react";
 
-const task = () => {
+const Task = () => {
+const BASE_URL = process.env.REACT_APP_BASE_URL;
+    const [task, setTask] = useState([]);
+    useEffect(() => {
+     taskshow()
+    }, [])
+    const taskshow = async () => {
+        const result = await axios.get(`${BASE_URL}/gettask`);
+       setTask(result.data);
+        
+    }
+
+
     return (
         <div>
             
+{task.map((e)=>(
+
+
+<ul>
+<li>{e.name}</li>
+
+</ul>
+))}
+
         </div>
     )
 }
 
-export default task
+export default Task
