ead440ad1e6bcf877296d5a13eae62657718503b
Sultan1901 <sultan.aqeel.elq.js@tuwaiq.edu.sa>
2021-12-05 23:01:48 +0300

    add localStorage

--- todo/src/component/Login/index.js
+++ todo/src/component/Login/index.js
@@ -3,20 +3,33 @@ import axios from "axios";
 import cors from "cors";
 import { useState, useEffect } from "react";
 
-
 const Login = () => {
-    const BASE_URL = process.env.REACT_APP_BASE_URL;
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
+  const BASE_URL = process.env.REACT_APP_BASE_URL;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+   const [local, setLocal] = useState("");
 
-    const login = async () => {
+  const login = async () => {
+    try {
       const result = await axios.post(`${BASE_URL}/login`, {
         email,
         password,
       });
+      if (result.data.token) {
+        localStorage.setItem("token", result.data.token);
+         
+      }
 
       console.log(result.data.token);
+    } catch (error) {
+      console.log(error);
     }
+  };
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+
+    setLocal(token);
+  }, []);
   return (
     <div>
       <>
